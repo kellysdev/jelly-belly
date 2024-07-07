587525a8937436335d6eeae994f7702438f1d928
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-07-07 15:19:56 -0700

    Initial skeleton of app will render a search bar, a button for a random selection, and a list of jelly beans.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [beans, setBeans] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+
   return (
     <div className="App">
+      <h1 className="app-title">Jelly Beans</h1>
 
+      <SearchBar 
+        onSearchTermChange={(searchInput) => {
+          setSearchInput(searchInput);
+        }}
+        searchInput={searchInput}
+      />
+
+      <div className="find-bean-wrapper">
+          <p>OR</p>
+          <button>Find a Random Jelly Bean</button>
+      </div>
+
+      <div className="bean-list">
+        {
+          beans.filter((bean) => {
+            if (!searchInput) {
+              return true;
+            } else {
+              return bean.flavorName.toLowerCase().includes(searchInput.toLowerCase());
+            }
+          }).map((bean) => (
+            <BeanButton bean={bean} />
+          ))
+        }
+      </div>
     </div>
   );
 }
