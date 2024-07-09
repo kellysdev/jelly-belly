2cb4322c5a196a99a91bde91f2e92700785d999e
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-07-08 17:01:08 -0700

    Style bean buttons to just render the bean image and flavor name.

--- src/components/bean-button/bean-button.js
+++ src/components/bean-button/bean-button.js
@@ -1,7 +1,12 @@
+import "../bean-button/bean-button.css"
+
 export const BeanButton = ({ bean }) => {
   return (
     <button className="bean-button">
-      <img src={bean.imageUrl} alt={bean.flavorName} />
+      <img 
+        src={bean.imageUrl} alt={bean.flavorName}
+        className="bean-image"
+      />
       {bean.flavorName}
     </button>
   );
