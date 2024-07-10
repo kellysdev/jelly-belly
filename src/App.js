769063fa7151ba13fe2c5113c732eeb052a9bca4
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-07-10 14:36:30 -0700

    Make box-shadow it's own class and apply to random button and disclaimer.

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
           <h2 className="lobster">or</h2>
 
           <button 
-            className="randomButton"
+            className="randomButton box-shadow"
             onClick={getRandomBean}
           >
             Find a Random Jelly Bean
@@ -82,7 +82,7 @@ function App() {
         }
       </div>
 
-      <footer className="disclaimer">
+      <footer className="disclaimer box-shadow">
         <p>
           All data in this application and its API are sourced from the official Jelly Belly website. None of the content here is claimed as original; it's all credited to and derived from Jelly Belly's official resources.
         </p>
