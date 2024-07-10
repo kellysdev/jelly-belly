7006ac1291da1e996bea5f2d51e8f364d0679c05
Kelly Scott <116105463+kellysdev@users.noreply.github.com>
2024-07-10 14:34:05 -0700

    Add a disclaimer regarding data coming from Jelly Belly.

--- src/App.js
+++ src/App.js
@@ -82,6 +82,12 @@ function App() {
         }
       </div>
 
+      <footer className="disclaimer">
+        <p>
+          All data in this application and its API are sourced from the official Jelly Belly website. None of the content here is claimed as original; it's all credited to and derived from Jelly Belly's official resources.
+        </p>
+      </footer>
+
       <Modal 
         showModal={showModal} closeModal={closeModal} 
         singleBean={singleBean}
